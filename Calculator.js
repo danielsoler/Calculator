e1cfff1e34d1ebd4b5f609356c8c956e2c6d65fd
Daniel <cdsg.6.20.13@gmail.com>
2018-01-02 19:52:18 -0500

    Arreglos

--- Calculator.js
+++ Calculator.js
@@ -1,16 +1,16 @@
 var num1 = num1
 var num2 = num2
-var oper = prompt("que operacion vas a hacer (suma, resta, multiplicacion o division)")
+var oper = prompt("Para sumar marque 1.\nPara restar marque 2.\nPara multiplicar marque 3.\nPara dividir marque 4.")
 
 num1 = prompt("por favor escriba un numero")
 num2 = prompt("por favor escriba otro numero")
 
-if (oper == "suma") {
+if (oper == "1") {
 	document.write(parseInt(num1)+parseInt(num2))
-} else if (oper == "resta") {
+} else if (oper == "2") {
 	document.write(parseInt(num1)-parseInt(num2))
-} else if (oper == "multiplicacion") {
+} else if (oper == "3") {
 	document.write(parseInt(num1)*parseInt(num2))
-} else if (oper == "division") {
+} else if (oper == "4") {
 	document.write(parseInt(num1)/parseInt(num2))
 };
